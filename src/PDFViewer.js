cb38d3e90bad099cce9d0f330d6c31fa0024084e
beomjourr <beomjourr@gmail.com>
2023-09-08 13:18:02 +0900

    test1

--- src/PDFViewer.js
+++ src/PDFViewer.js
@@ -2,47 +2,41 @@ import React, { useState, useRef } from 'react';
 import { Document, Page } from 'react-pdf';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/swiper-bundle.css';
-import { usePinch } from '@use-gesture/react';
 import file from './test3.pdf';
+import 'swiper/css/zoom';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import { nanoid } from 'nanoid'
 
+import { Zoom, Navigation } from 'swiper/modules';
 function PDFViewer() {
   const [numPages, setNumPages] = useState(null);
   const [pageNumber, setPageNumber] = useState(1);
-  const [scale, setScale] = useState(1);
   const [isTwoPageView, setIsTwoPageView] = useState(false);
-  const [position, setPosition] = useState({ x: 0, y: 0 });
-  const viewerRef = useRef(null);
+  const swiperRef = useRef(null);
 
-  const MIN_SCALE = 1;
-  const MAX_SCALE = 3;
+  const handleZoomChange = (scale) => {
+    const swiper = swiperRef.current?.swiper;
 
-  const bind = usePinch(({ da: [dX, dY], origin: [ox, oy], memo }) => {
-    const newScale = Math.max(MIN_SCALE, Math.min(scale * (1 + dY / window.innerHeight), MAX_SCALE));
+    if (!swiper) return;
 
-    const viewer = viewerRef.current;
-    const rect = viewer.getBoundingClientRect();
-    const originX = ox - rect.left - position.x;
-    const originY = oy - rect.top - position.y;
-
-    setPosition({
-      x: position.x - (originX * newScale - originX),
-      y: position.y - (originY * newScale - originY),
-    });
-
-    setScale(newScale);
-    return { originX: ox, originY: oy };
-  });
+    if (swiper.zoom.scale <= 1) {
+        // Content is zoomed in, so disable swiper
+        swiper.allowSlideNext = false;
+        swiper.allowSlidePrev = false;
+    } else {
+        // Content is zoomed out, so enable swiper
+        swiper.allowSlideNext = true;
+        swiper.allowSlidePrev = true;
+    }
+};
 
   const onDocumentLoadSuccess = ({ numPages }) => {
     setNumPages(numPages);
   };
 
   return (
-    <div ref={viewerRef} {...bind()} style={{
-      transform: `translate3d(${position.x}px, ${position.y}px, 0) scale(${scale})`,
-      transformOrigin: '0 0',
-      touchAction: 'none'
-    }}>
+    <div>
       <Document
         file={file}
         onLoadSuccess={onDocumentLoadSuccess}
@@ -52,12 +46,18 @@ function PDFViewer() {
           spaceBetween={10}
           pagination={{ clickable: true }}
           onSlideChange={({ activeIndex }) => setPageNumber(activeIndex + 1)}
+          zoom={true}
+          onZoomChange={handleZoomChange}
+          ref={swiperRef}
+          modules={[Zoom, Navigation]}
         >
-          {Array.from(new Array(numPages || 0), (el, index) => (
-            <SwiperSlide key={index}>
-              <Page pageNumber={index + 1} scale={scale} renderAnnotationLayer={false} renderTextLayer={false} />
-            </SwiperSlide>
-          ))}
+            {Array.from(new Array(numPages || 0), (el, index) => (
+              <SwiperSlide key={nanoid()}>
+                <div className="swiper-zoom-container">
+                  <Page className="canvas-page" pageNumber={index + 1} renderAnnotationLayer={false} renderTextLayer={false} />
+                </div>
+              </SwiperSlide>
+            ))}
         </Swiper>
         <p>Current Page: {pageNumber}</p>
         <button onClick={() => setIsTwoPageView(prev => !prev)}>
